32bc66965bd207c9b75579cb2280ec2683f5fb62
Hrithika56 <159103095+Hrithika56@users.noreply.github.com>
2024-04-15 12:25:13 +0530

    yes

--- server.mjs
+++ server.mjs
@@ -1,41 +1,45 @@
+
 import { createServer } from 'node:http';
- const hostname='127.0.0.1';
- const port=3000;
- const server = createServer(( req, res) => {
-    console.log('recevied request ....', req.url, req.method);
 
-    if (req.url=='/');
-    if(req.method=='Get') {
-        res.statusCode=200;
-        res.setHeader('content-Type', 'text/plain');
-        res.setHeader('Acess-Control-Allow-Origin' , "*");
+const hostname = '127.0.0.1';
+const port = 3000;
 
-        res.end("Mithilesh ");
-        //console.log(res)
+const server = createServer((req, res) => {
+  console.log('Received request...', req.url, req.method);
+  
+  if(req.url == '/') {
+    if(req.method == 'GET') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Access-Control-Allow-Origin', "*");
+
+
+      res.end("Mithilesh");
+      // console.log(res)
     }
-    if( req.method == 'option'){
-        res.statusCode=200;
-        res.setHeader('content-Type ', 'application/json' );
-        res.setHeader('Acess-Control-Allow-Orign ', 'http://127.0.0.1.5500');
-        res.setHeader('Acess-Control-Allow-Methods  ', "POST ", GET, OPTIONS);
-        res.setHeader('Acess-Control-Allow-Header ', '* ');
-        res.end('OK');
-
+    if(req.method == 'OPTIONS') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'appliction/json');
+      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+      res.setHeader('Access-Control-Allow-Methods', "POST, GET, OPTIONS");
+      res.setHeader('Access-Control-Allow-Headers', '*');
+      res.end('OK');
     }
+    if(req.method == 'POST') {
+      // console.log(req.body());
+      console.log('post method...')
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Access-Control-Allow-Origin', "*");
 
-    if(req.method=='post') {
-        //console.log(req.body ());
-        console.log('postmethod ....')
-        res.statusCode=200;
-        res.setHeader('content-Type ', 'text/plan ');
-        res.setHeader('Acess-Control-Allow-Orgin', "*");
 
-        res.end(JSON. stringify( { surname :"KUMAR"}));
-
+      res.end(JSON.stringify({surname: "KUMAR"}));
+      // console.log(res)
     }
- }
+  }
+  
 });
 
-server.listen(port,hostname, ()=> {
-    console.log('server running at http ')
-})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
