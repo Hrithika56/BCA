772ff530a461e2b11cf5b753662d37469de441aa
Hrithika56 <159103095+Hrithika56@users.noreply.github.com>
2024-04-18 14:19:23 +0530

    yes

--- http.js
+++ http.js
@@ -1,3 +1,4 @@
+
 let nameElement = document.querySelector('.name');
 let surnameElement = document.querySelector('.surname')
 let surnameInput = document.querySelector('#surnameInput');
@@ -13,14 +14,15 @@ const getMyName = async () => {
     nameElement.textContent = name;
 }
 const postSurname = async () => {
+    let mysurname = surnameInput.value;
     let res = await fetch('http://127.0.0.1:3000/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({surname: 'Kumar'})
+        body: JSON.stringify({surname: mysurname})
     })
-    console.log(res);
+    // console.log(res);
     if(!res.ok) {
         alert('Failed to post surname');
         return;
